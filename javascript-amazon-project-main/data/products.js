e7eb11e97211533df261a1cb3fe9285dd2b55d12
vicky <eaintvickyyang@gmail.com>
2025-03-17 00:06:36 +0700

    learn more details about classes and this

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -93,8 +93,34 @@ const tshirt = new Clothing({
   sizeChartLink: "images/clothing-size-chart.png"
 });
 
-console.log(tshirt);
-console.log(tshirt.getPrice());
+/*
+const date = new Date();
+console.log(date.toLocaleTimeString());
+*/
+
+/*
+console.log(this);
+
+const object2 = {
+  a: 2,
+  b: this.a
+}; */
+
+/*
+function logThis() {
+  console.log(this);
+}
+logThis();
+logThis.call('hello');
+
+this
+const object3 = {
+  method: () => {
+    console.log(this);
+  }
+}
+object3.method();
+*/
 
 export const products = [
   {
